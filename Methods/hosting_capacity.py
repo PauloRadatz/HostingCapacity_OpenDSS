76e3808c0a7af588652d0e8dc6b2dd767a090dba
PauloRadatz <pradatz@epri.com>
2022-11-29 15:08:22 -0500

    Working on simple circuit

--- Methods/hosting_capacity.py
+++ Methods/hosting_capacity.py
@@ -8,23 +8,50 @@
 
 class HostingCapacity:
 
-    def __init__(self, dss, bus, load_mult):
+    def __init__(self, dss_file, dss, bus, offpeak_load_mult, peak_load_mult, max_p, p_step):
+        self.dss_file = dss_file
         self.dss = dss
         self.bus = bus
-        self.load_mult = load_mult
+        self.offpeak_load_mult = offpeak_load_mult
+        self.peak_load_mult = peak_load_mult
+        self.max_p = max_p
+        self.p_step = p_step
 
-    def ov_criteria_hc_calc(self, gen_kv, ov_threshold_pu, p_step, max_p=10000):
-        self.__new_gen(gen_kv, p_step)
+    def set_feeder(self):
+        # self.dss.text(f"Compile [{self.dss_file}]")
+        # self.dss.text("Set Maxiterations=100")
+        # self.dss.text("Edit Vsource.Source pu=1.03")
+        # self.dss.text("edit Reactor.MDV_SUB_1_HSB x=0.0000001")
+        # self.dss.text("edit Transformer.MDV_SUB_1 %loadloss=0.0000001 xhl=0.00000001")
+        # self.dss.text("Set controlmode=Off")
+
+        self.dss.text(f"Compile [{self.dss_file}]")
+        # self.dss.text("Set Maxiterations=100")
+        # self.dss.text("Set Maxcontroli=100")
+        # self.dss.text("Edit Vsource.Source pu=1.04")
+        # self.dss.text("edit Reactor.MDV_SUB_1_HSB x=0.0000001")
+        # self.dss.text("edit Transformer.MDV_SUB_1 %loadloss=0.0000001 xhl=0.00000001")
+        # self.dss.text("Set controlmode=Off")
+        # self.dss.text("batchedit load..* mode=1")
+        # self.dss.text("batchedit load..* vmaxpu=1.25")
+        # self.dss.text("batchedit load..* vminpu=0.75")
+        # self.dss.text("New Energymeter.m1 Line.ln5815900-1 1")
+        # self.dss.text("batchedit capacitor..* enabled=no")
+
+
+    def ov_gen_hc_calc(self, gen_kv, ov_threshold_pu):
+        self.set_feeder()
+        self.__new_gen(gen_kv, self.p_step)
 
         i = 0
         ov_violation = False
 
-        self.dss.text(f"set loadmult={self.load_mult}")
+        self.dss.text(f"set loadmult={self.offpeak_load_mult}")
         self.dss.text("solve")
         # self.dss.text("Plot profile phases=all")
-        while not ov_violation and i * p_step <= max_p:
+        while not ov_violation and i * self.p_step <= self.max_p:
             i += 1
-            self.__increment_generator_size(p_step * i)
+            self.__increment_generator_size(self.p_step * i)
             self.dss.text("solve")
             # self.dss.text("Plot profile phases=all")
             max_v = self.__get_max_feeder_voltage()
@@ -33,9 +60,114 @@ class HostingCapacity:
                 ov_violation = True
 
         if ov_violation:
-            return (i - 1) * p_step
+            return (i - 1) * self.p_step
         else:
-            return max_p
+            return self.max_p
+
+    def ol_gen_hc_calc(self, gen_kv):
+        self.set_feeder()
+        self.__new_gen(gen_kv, self.p_step)
+
+        i = 0
+        ol_violation = False
+
+        self.dss.text(f"set loadmult={self.offpeak_load_mult}")
+        self.dss.text("solve")
+        # self.dss.text("Plot profile phases=all")
+        while not ol_violation and i * self.p_step <= self.max_p:
+            i += 1
+            self.__increment_generator_size(self.p_step * i)
+            self.dss.text("solve")
+            # self.dss.text("Plot profile phases=all")
+            for element in self.dss.circuit_all_element_names():
+                if element != "Vsource.source":
+                    if element.split(".")[0] == "Line":
+                        self.dss.circuit_set_active_element(element)
+                        rating_current = self.dss.cktelement_read_norm_amps()
+                        element_currents = self.dss.cktelement_currents_mag_ang()
+
+                        if self.dss.cktelement_num_phases() == 3:
+                            max_phase_current = max(element_currents[0:12:2])
+                        elif self.dss.cktelement_num_phases() == 2:
+                            max_phase_current = max(element_currents[0:8:2])
+                        elif self.dss.cktelement_num_phases() == 1:
+                            max_phase_current = max(element_currents[0:4:2])
+
+                        if rating_current > 0:
+                            if max_phase_current / rating_current > 1:
+                                print(f"OL Gen element = {element}")
+                                ol_violation = True
+                                break
+
+        if ol_violation:
+            return (i - 1) * self.p_step
+        else:
+            return self.max_p
+
+    def uv_load_hc_calc(self, load_kv, uv_threshold_pu):
+        self.set_feeder()
+        self.__new_load(load_kv, self.p_step)
+
+        i = 0
+        uv_violation = False
+
+        self.dss.text(f"set loadmult={self.peak_load_mult}")
+        self.dss.text("solve")
+        # self.dss.text("Plot profile phases=all")
+        while not uv_violation and i * self.p_step <= self.max_p:
+            i += 1
+            self.__increment_load_size(self.p_step * i)
+            self.dss.text("solve")
+            # self.dss.text("Plot profile phases=all")
+            min_v = self.__get_min_feeder_voltage()
+
+            if min_v < uv_threshold_pu:
+                uv_violation = True
+
+        if uv_violation:
+            return (i - 1) * self.p_step
+        else:
+            return self.max_p
+
+    def ol_load_hc_calc(self, load_kv):
+        self.set_feeder()
+        self.__new_load(load_kv, self.p_step)
+
+        i = 0
+        ol_violation = False
+
+        self.dss.text(f"set loadmult={self.peak_load_mult}")
+        self.dss.text("solve")
+        # self.dss.text("Plot profile phases=all")
+        while not ol_violation and i * self.p_step <= self.max_p:
+            i += 1
+            self.__increment_load_size(self.p_step * i)
+            self.dss.text("solve")
+            # self.dss.text("Plot profile phases=all")
+            for element in self.dss.circuit_all_element_names():
+                if element != "Vsource.source":
+                    if element.split(".")[0] == "Line":
+                        self.dss.circuit_set_active_element(element)
+                        rating_current = self.dss.cktelement_read_norm_amps()
+                        element_currents = self.dss.cktelement_currents_mag_ang()
+
+                        if self.dss.cktelement_num_phases() == 3:
+                            max_phase_current = max(element_currents[0:12:2])
+                        elif self.dss.cktelement_num_phases() == 2:
+                            max_phase_current = max(element_currents[0:8:2])
+                        elif self.dss.cktelement_num_phases() == 1:
+                            max_phase_current = max(element_currents[0:4:2])
+
+                        if rating_current > 0:
+                            if max_phase_current / rating_current > 1:
+                                print(f"OL Load element = {element}")
+                                ol_violation = True
+                                break
+
+        if ol_violation:
+            return (i - 1) * self.p_step
+        else:
+            return self.max_p
 
     def __new_gen(self, gen_kv, kw):
         self.dss.text(f"new generator.gen "
@@ -46,14 +178,31 @@ class HostingCapacity:
                       f"kva={kw} "
                       f"pf=1")
 
+    def __new_load(self, load_kv, kw):
+        self.dss.text(f"new load.load "
+                      f"phases=3 "
+                      f"kv={load_kv} "
+                      f"bus1={self.bus} "
+                      f"kw={kw} "
+                      f"pf=1 "
+                      f"status=Fixed")
+
     def __increment_generator_size(self, kw):
         self.dss.text(f"edit generator.gen "
                       f"kw={kw} "
                       f"kva={kw}")
 
+    def __increment_load_size(self, kw):
+        self.dss.text(f"edit load.load "
+                      f"kw={kw}")
+
     def __get_max_feeder_voltage(self):
         voltages = self.dss.circuit_all_bus_vmag_pu()
         return max(voltages)
+
+    def __get_min_feeder_voltage(self):
+        voltages = self.dss.circuit_all_bus_vmag_pu()
+        return min(voltages)
 
 
 
--- process_tshc.py
+++ process_tshc.py
@@ -1,32 +1,15 @@
 # -*- coding: utf-8 -*-
 # @Author  : Paulo Radatz
 # @Email   : pradatz@epri.com
-# @File    : process_hc.py
+# @File    : process_tshc.py
 # @Software: PyCharm
 
-import py_dss_interface  # YouTube videos: https://www.youtube.com/playlist?list=PLhdRxvt3nJ8zlzp6b_-7s3_YwwlunTNRC
+import py_dss_interface
 import pathlib
 import os
 import math
 import pandas as pd
 from Methods import HostingCapacity
-
-# def get_3ph_mv_buses():
-#     mv_buses = list()
-#     mv_bus_voltage_dict = dict()
-#
-#     buses = dss.circuit_all_bus_names()
-#
-#     for bus in buses:
-#         dss.circuit_set_active_bus(bus)
-#         if bus == "sourcebus":
-#             pass
-#         elif dss.bus_kv_base() >= 1.0 and len(dss.bus_nodes()) == 3:
-#             mv_buses.append(bus)
-#             mv_bus_voltage_dict[bus] = dss.bus_kv_base() * math.sqrt(3)
-#
-#     return mv_buses, mv_bus_voltage_dict
-
 
 script_path = os.path.dirname(os.path.abspath(__file__))
 dss_file = pathlib.Path(script_path).joinpath("Feeders", "SimpleCircuit", "SimpleCircuit.dss")
@@ -41,12 +24,16 @@ ov_gen_hc_list = []
 uv_load_hc_list = []
 ol_gen_hc_list = []
 ol_load_hc_list = []
-mv_buses = ["C"]
-for bus in mv_buses:
-    dss.circuit_set_active_bus(bus)
-    bus_kv = dss.bus_kv_base() * math.sqrt(3)
-    bus_name_list.append(bus)
-    hc_obj = HostingCapacity(dss_file, dss, bus, 0.3, 1, 20000, 250)
+bus = "C"
+
+dss.circuit_set_active_bus(bus)
+bus_kv = dss.bus_kv_base() * math.sqrt(3)
+bus_name_list.append(bus)
+
+loadshape = [0.3, 0.32, 0.3, 0.3, 0.35, 0.42, 0.46, 0.59, 0.66, 0.88, 0.94, 0.98, 0.98, 0.98, 0.98, 0.99, 1, 0.95, 0.93, 0.91, 0.78, 0.69, 0.53, 0.47]
+
+for load_level in loadshape:
+    hc_obj = HostingCapacity(dss_file, dss, bus, load_level, load_level, 20000, 250)
     ov_gen_hc_list.append(hc_obj.ov_gen_hc_calc(bus_kv, 1.05))
     uv_load_hc_list.append(hc_obj.uv_load_hc_calc(bus_kv, 0.95))
     ol_gen_hc_list.append(hc_obj.ol_gen_hc_calc(bus_kv))
@@ -54,7 +41,7 @@ for bus in mv_buses:
 
 # Save results in a csv file
 dict_to_df = dict()
-dict_to_df["Bus_name"] = bus_name_list
+# dict_to_df["Bus_name"] = bus_name_list
 dict_to_df["OV Gen HC (kW)"] = ov_gen_hc_list
 dict_to_df["OL Gen HC (kW)"] = ol_gen_hc_list
 dict_to_df["UV Load HC (kW)"] = uv_load_hc_list
@@ -62,7 +49,5 @@ dict_to_df["OL Load HC (kW)"] = ol_load_hc_list
 
 df = pd.DataFrame().from_dict(dict_to_df)
 
-output_file = pathlib.Path(script_path).joinpath("outputs", "results.csv")
+output_file = pathlib.Path(script_path).joinpath("outputs", "results_tshc.csv")
 df.to_csv(output_file, index=False)
-
-
